06f08ceff84f79791edb1ccc62ac81f29bacc2f9
Barbara Ferraz <babihferraz21@gmail.com>
2022-12-06 18:48:20 -0300

    Migrations

---
+++ 07_RestWithAspNetUdemy_Migrations/RestWithAspNetUdemy/RestWithAspNetUdemy/Model/Books.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("books")]
+    public class Books
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("author")]
+        public string FirstName { get; set; }
+
+        [Column("launch_date")]
+        public string LastName { get; set; }
+
+        [Column("price")]
+        public string Address { get; set; }
+        [Column("title")]
+        public string Gender { get; set; }
+    }
+}
