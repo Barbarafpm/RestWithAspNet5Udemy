84166afea17ba3077315a0fe1757103d6312755d
Barbara Ferraz <babihferraz21@gmail.com>
2022-12-08 14:52:31 -0300

    HATEOAS

---
+++ 12_RestWithAspNetUdemy_Hateoas/RestWithAspNetUdemy/RestWithAspNetUdemy/Hypermedia/Abstrct/ISupportHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Hypermedia.Abstrct
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediLink> Links { get; set; }
+    }
+}
