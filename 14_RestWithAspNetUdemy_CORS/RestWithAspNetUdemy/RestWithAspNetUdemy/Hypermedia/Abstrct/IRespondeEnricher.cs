8ac65fb832756a1e89fb9578d4cef9b30ef4fbb8
Barbara Ferraz <babihferraz21@gmail.com>
2022-12-14 14:31:42 -0300

    CORS

---
+++ 14_RestWithAspNetUdemy_CORS/RestWithAspNetUdemy/RestWithAspNetUdemy/Hypermedia/Abstrct/IRespondeEnricher.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Hypermedia
+{
+    public interface IRespondeEnricher
+    {
+        bool CanEnrich(ResultExecutingContext context);
+
+    }
+}
