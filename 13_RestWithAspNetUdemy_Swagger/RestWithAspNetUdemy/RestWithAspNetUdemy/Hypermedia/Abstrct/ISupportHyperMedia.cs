34bb66b99b9d0f98bb77b5d8b43e4f358535a3b8
Barbara Ferraz <babihferraz21@gmail.com>
2022-12-14 14:20:10 -0300

    Swagger

---
+++ 13_RestWithAspNetUdemy_Swagger/RestWithAspNetUdemy/RestWithAspNetUdemy/Hypermedia/Abstrct/ISupportHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Hypermedia.Abstrct
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
